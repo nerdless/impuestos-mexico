37a2ea763157a5a40d74fc818975b05dd009dbce
Eduardo Ochoa <eochoa@intelimetrica.com>
2022-08-19 01:39:05 -0500

    Adding dta to isr table

--- migrations/versions/e3ce8dd4d4e6_load_isr_mensual_data.py
+++ migrations/versions/e3ce8dd4d4e6_load_isr_mensual_data.py
@@ -8,6 +8,7 @@ Create Date: 2022-08-18 20:33:19.268402
 from alembic import op
 import sqlalchemy as sa
 import os
+from csv import reader
 
 
 
@@ -17,17 +18,21 @@ down_revision = '685c42aa7421'
 branch_labels = None
 depends_on = None
 
-data_path = "files/isr_mensual.csv"
+data_path = "../../files/isr_mensual.csv"
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, data_path)
 
 def upgrade() -> None:
-    op.execute(f"""LOAD DATA INFILE '{filename}' 
-                    INTO TABLE isr_mensual 
-                    FIELDS TERMINATED BY ',' 
-                    ENCLOSED BY '"'
-                    LINES TERMINATED BY '\\n'
-                    IGNORE 1 ROWS""")
+    with open(filename, 'r') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        rows = list(csv_reader)
+        for row in rows[1:]:
+            # row variable is a list that represents a row in csv
+            query = f"""INSERT INTO isr_mensual (fecha, rango, limite_inferior, limite_superior, cuota, tasa)
+                       VALUES ('{"','".join(row)}')"""
+            op.execute(query)
 
 
 def downgrade() -> None:
